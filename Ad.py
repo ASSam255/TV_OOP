b946c9aedf9796052e63ffafda1ee4a9bf154477
ASSam255 <85800162+ASSam255@users.noreply.github.com>
2024-03-30 16:54:08 +0300

    Ad.py

---
+++ Ad.py
@@ -0,0 +1,32 @@
+from Telecast import Telecast
+from Client import Client
+
+class Ad():
+  __id = int
+  __telecast = Telecast
+  __client = Client
+  __date = str
+  __time = int
+
+  def __init__(self, id, telecast, client, date, time):
+    self.setID(id)
+    self.setTelecast(telecast)
+    self.setClient(client)
+    self.setDate(date)
+    self.setTime(time)
+
+  def setID(self, id): self.__id = id
+  def setTelecast(self, value): self.__telecast = value
+  def setClient(self, value): self.__client = value
+  def setDate(self, value): self.__date = value
+  def setTime(self, value): self.__time = value
+
+  def getID(self): return self.__id
+  def getTelecast(self): return self.__telecast
+  def getClient(self): return self.__client
+  def getDate(self): return self.__date
+  def getTime(self): return self.__time
+
+  def __str__(self):
+    return "ID: {}\nПередача: {}\nКлиент: {}\nДата показа: {}\nДлительность в минутах: {} ".format(
+            self.__id, self.__telecast, self.__client, self.__date, self.__time)
